9ce92b8947664a6a1d65409a809ffc2008840686
Matheus <matheus.augustoreal@gmail.com>
2023-02-08 09:09:06 -0300

    feat: avatar upload

---
+++ src/database/migrations/1675855624370-AlterUserAddAvatar.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class AlterUserAddAvatar1675855624370 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      "users",
+      new TableColumn({
+        name: "avatar",
+        type: "varchar",
+        isNullable: true,
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn("users", "avatar");
+  }
+}
