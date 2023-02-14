99a46551b035c1ba37510e60180ece993197bb6a
Matheus <matheus.augustoreal@gmail.com>
2023-02-14 14:17:18 -0300

    refactor: user auth

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "afbc583b1997bf40546fcf9e32a6184d",
+  expires_in_token: "15m",
+  secret_refresh_token: "1226002e30cf058c8d14e2a28d303304",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,46 +1,50 @@
-import { container } from 'tsyringe';
+import { container } from "tsyringe";
 
-import '@shared/container/providers';
+import "@shared/container/providers";
 
-import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
-import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
-import { CarsImagesRepository } from '@modules/cars/infra/typeorm/repositories/CarsImagesRepository';
-import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
-import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
-import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationRepository';
-import { ICarsImagesRepository } from '@modules/cars/repositories/ICarsImagesRepository';
-import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
-import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
-import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationRepository';
-import { RentalsRepository } from '@modules/rentals/infra/typeorm/repositories/RentalsRepository';
-import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
+import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarsImagesRepository";
+import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
+import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
+import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationRepository";
+import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
+import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationRepository";
+import { RentalsRepository } from "@modules/rentals/infra/typeorm/repositories/RentalsRepository";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { IUsersTokenRepository } from "@modules/accounts/repositories/IUsersTokenRepository";
+import { UsersTokenRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokenRepository";
 
 container.registerSingleton<ICategoriesRepository>(
-  'CategoriesRepository',
-  CategoriesRepository,
+  "CategoriesRepository",
+  CategoriesRepository
 );
 
 container.registerSingleton<ISpecificationsRepository>(
-  'SpecificationsRepository',
-  SpecificationsRepository,
+  "SpecificationsRepository",
+  SpecificationsRepository
 );
 
 container.registerSingleton<IUsersRepository>(
-  'UsersRepository',
-  UsersRepository,
+  "UsersRepository",
+  UsersRepository
 );
 
-container.registerSingleton<ICarsRepository>(
-  'CarsRepository',
-  CarsRepository,
-);
+container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
 
 container.registerSingleton<ICarsImagesRepository>(
-  'CarsImagesRepository',
-  CarsImagesRepository,
+  "CarsImagesRepository",
+  CarsImagesRepository
 );
 
 container.registerSingleton<IRentalsRepository>(
-  'RentalsRepository',
-  RentalsRepository,
+  "RentalsRepository",
+  RentalsRepository
+);
+
+container.registerSingleton<IUsersTokenRepository>(
+  "UsersTokenRepository",
+  UsersTokenRepository
 );
